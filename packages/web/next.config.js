663dcf06f65651c59a421886c01f1994bc623912
Tomiez <tom.hiepnguyen@gmail.com>
2022-06-07 11:27:22 +0700

    feat(web): config babel, next, ts to use shared package

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const path = require('path');
+const withTM = require('next-transpile-modules')(['@frms/shared'], {
+  resolveSymlinks: true,
+});
 
-module.exports = nextConfig;
+module.exports = withTM({
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'react-native$': 'react-native-web',
+    };
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions,
+    ];
+    config.resolve.alias['react-native-web'] = path.resolve(
+      __dirname,
+      '.',
+      'node_modules',
+      'react-native-web',
+    );
+
+    return config;
+  },
+});
