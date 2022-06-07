706a5696ddd416abe11c01977970f1db790b422b
Tomiez <tom.hiepnguyen@gmail.com>
2022-06-07 11:29:42 +0700

    feat(web): dummy use elements in shared package

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Welcome } from '@frms/shared';
+
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
@@ -13,7 +15,7 @@ const Home: NextPage = () => (
 
     <main className={styles.main}>
       <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <Welcome />
       </h1>
 
       <p className={styles.description}>
