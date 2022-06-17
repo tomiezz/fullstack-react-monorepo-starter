f0bfda958f297714be7d21360fc85df98b12de29
Tomiez <tom.hiepnguyen@gmail.com>
2022-06-17 15:43:35 +0700

    feat(ci/cd): build web img

--- scripts/build-img.sh
+++ scripts/build-img.sh
@@ -1,11 +1,5 @@
 set -e
 
-CURRENT_TAG="$CI_COMMIT_SHA"
-IMG_NAME_BY_ENV="$IMG_NAME/${CURRENT_ENV}"
-if [ ! -z "$CI_COMMIT_TAG" ]; then
-    CURRENT_TAG="$CI_COMMIT_TAG"
-fi
-
 build_image() {
     IMG_NAME=$1
     CUSTOM_TAG=$2
@@ -19,4 +13,18 @@ build_image() {
     docker push $IMG_NAME:latest
 }
 
-build_image $IMG_NAME_BY_ENV $CURRENT_TAG
+CURRENT_TAG="$CI_COMMIT_SHA"
+CURRENT_ENV=development
+
+if [ "$CI_COMMIT_REF_NAME" = "dev" ]; then
+    CURRENT_ENV=development
+elif [ "$CI_COMMIT_REF_NAME" = "stag" ]; then
+    CURRENT_ENV=staging
+else
+    CURRENT_ENV=production
+    CURRENT_TAG="$CI_COMMIT_TAG"
+fi
+
+CURRENT_IMG_NAME="$IMG_NAME/$CURRENT_ENV"
+
+build_image $CURRENT_IMG_NAME $CURRENT_TAG
